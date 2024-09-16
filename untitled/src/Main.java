94e186e23447285102ca4ad49f4ea21f3bfd056a
Florb11 <florbergman11@gmail.com>
2024-09-16 12:34:40 -0300

    commit

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,57 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Proyecto proyecto1 = new Proyecto();
+
+        String[] menu = {"Crear proyecto","Agregar etapa","Revisr proyecto","Ver etapas","Salir"};
+        int opcion = 0;
+        do {
+            opcion = JOptionPane.showOptionDialog(null, "Elija una opción","Menu", JOptionPane.DEFAULT_OPTION, 0, new ImageIcon(Main.class.getResource("/img/conts.png")), menu, menu[0]);
+            switch (opcion) {
+                case 0:
+                    if (proyecto1.getNombre().equals("No asignado")) {
+                        JOptionPane.showMessageDialog(null, "Creando proyecto");
+                        proyecto1.registrarProyecto();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "El proyecto ya esta creado");
+                    }
+
+                    break;
+                case 1:
+                    if (proyecto1.getNombre().equals("No asignado")) {
+                        JOptionPane.showMessageDialog(null, "Todavía no se creó un proyecto");
+                    } else {
+                        proyecto1.evaluarEtapas();
+                    }
+
+
+                    break;
+                case 2:
+                    if (proyecto1.getNombre().equals("No asignado")) {
+                        JOptionPane.showMessageDialog(null, "Todavía no se creó un proyecto");
+                    } else {
+                        JOptionPane.showMessageDialog(null, proyecto1);
+                    }
+
+
+
+                    break;
+                case 3:
+                    if (proyecto1.getNombre().equals("No asignado")) {
+                        JOptionPane.showMessageDialog(null, "Todavía no se creó un proyecto");
+                    } else {
+                        JOptionPane.showMessageDialog(null, proyecto1.getEtapas());
+                    }break;
+
+                case 4:
+                    JOptionPane.showMessageDialog(null, "A bueno adios master", "fsdf", JOptionPane.DEFAULT_OPTION , null);
+                    break;
+            }
+
+        } while (opcion!=4);
+
+
     }
-}
+
+    }
--- untitled/src/Proyecto.java
+++ untitled/src/Proyecto.java
@@ -7,18 +7,26 @@ public class Proyecto {
     private String ubicacion;
     private String cliente;
     private LocalDate fechaInicio;
-    private LocalDate fechaFin;
+    private LocalDate progreso;
     private String etapas;
 
     //constructor
 
-    public Proyecto(String nombre,String ubicacion,String cliente,LocalDate fechaInicio, LocalDate fechaFin,String etapas){
+    public Proyecto(String nombre,String ubicacion,String cliente,LocalDate fechaInicio, LocalDate progreso,String etapas){
         this.nombre=nombre;
         this.ubicacion=ubicacion;
         this.cliente=cliente;
         this.fechaInicio=fechaInicio;
-        this.fechaFin=fechaFin;
+        this.progreso=progreso;
         this.etapas=etapas;
+    }
+    public Proyecto() {
+        this.nombre = "No asignado";
+        this.ubicacion = "No asignado";
+        this.cliente = "No asignado";
+        this.fechaInicio = LocalDate.now();
+        this.etapas = "Etapas:";
+        this.progreso = LocalDate.now();  // para poder crear el objeto vacio
     }
     //get y set
 
@@ -39,11 +47,11 @@ public class Proyecto {
         return fechaInicio;
     }
 
-    public LocalDate getFechaFin() {
-        return fechaFin;
+    public LocalDate getProgreso() {
+        return progreso;
     }
 
-    public int getEtapas() {
+    public String getEtapas() {
         return etapas;
     }
 
@@ -63,8 +71,8 @@ public class Proyecto {
         this.fechaInicio = fechaInicio;
     }
 
-    public void setFechaFin(LocalDate fechaFin) {
-        this.fechaFin = fechaFin;
+    public void setProgreso(LocalDate fechaFin) {
+        this.progreso = fechaFin;
     }
 
     public void setEtapas(String etapas) {
@@ -77,9 +85,26 @@ public class Proyecto {
         this.ubicacion =JOptionPane.showInputDialog(null,"Ingrese la ubicacion del proyecto");
         this.cliente=JOptionPane.showInputDialog(null,"Ingrese el cliente");
         this.fechaInicio= LocalDate.parse(JOptionPane.showInputDialog(null,"Ingrese la fecha de inicio del proyecto"));
+        this.progreso= this.fechaInicio;
 
     }
     public void evaluarEtapas(){
+        String etapa = JOptionPane.showInputDialog(null,"Ingrese la etapa");
+        int calificacion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la califacion "));
+        this.progreso = this.fechaInicio.plusDays(1);
+        this.etapas = this.etapas + "Etapa: "+ etapa + "\n" + "La calificacion es: "+calificacion +"Progreso: " + this.progreso +"dias";
 
+    }
+
+    @Override
+    public String toString() {
+        return "Proyecto{" +
+                "nombre='" + nombre + '\'' +
+                ", ubicacion='" + ubicacion + '\'' +
+                ", cliente='" + cliente + '\'' +
+                ", fechaInicio=" + fechaInicio +
+                ", progreso=" + progreso +
+                ", etapas='" + etapas + '\'' +
+                '}';
     }
 }
